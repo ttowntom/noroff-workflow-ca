a40c175869bfee7610ec31d6192a93f73d4271ad
ttowntom <git.running054@passinbox.com>
2024-06-03 15:44:02 +0200

    adds e2e testing for invalid credentials

---
+++ cypress/e2e/1-login/loginInvalid.cy.js
@@ -0,0 +1,26 @@
+describe("Noroff social invalid login (not a Noroff email address)", () => {
+  it("can log in with the login form with valid credentials", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.wait(500);
+    cy.get('button[data-auth="login"]').last().click();
+    cy.wait(500);
+    cy.fixture("loginDataInvalid.json").then((loginData) => {
+      cy.get("input#loginEmail").type(loginData.email);
+      cy.get("input#loginPassword").type(loginData.password);
+    });
+    cy.get('button[type="submit"]').contains("Login").click();
+    cy.get("form#loginForm").then(($form) => {
+      if ($form[0].checkValidity()) {
+        // Form is valid, no validation error
+      } else {
+        // Form is invalid, validation error has been triggered
+        cy.get("input#loginEmail").then(($input) => {
+          expect($input[0].validationMessage).to.eq(
+            // The error message is in Norwegian
+            "Sørg for samsvar med det forespurte formatet.",
+          );
+        });
+      }
+    });
+  });
+});
---
+++ cypress/e2e/1-login/loginInvalid401.cy.js
@@ -0,0 +1,20 @@
+describe("Noroff social invalid login (wrong password)", () => {
+  it("can log in with the login form with valid credentials", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.wait(500);
+    cy.get('button[data-auth="login"]').last().click();
+    cy.wait(500);
+    cy.fixture("loginData401.json").then((loginData) => {
+      cy.get("input#loginEmail").type(loginData.email);
+      cy.get("input#loginPassword").type(loginData.password);
+    });
+    cy.intercept(
+      "POST",
+      "https://nf-api.onrender.com/api/v1/social/auth/login",
+    ).as("loginRequest");
+    cy.get('button[type="submit"]').contains("Login").click();
+    cy.wait("@loginRequest").then((interception) => {
+      expect(interception.response.statusCode).to.eq(401);
+    });
+  });
+});
